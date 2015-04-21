36a086ded770cc540317b9f50268d49a51de9ee0
Austin Li <austinli@Austins-MacBook-Pro.local>
2015-04-20 21:30:48 -0700

    Moved changeable variables to seperate file. Implemented the semantic analysis API and made some formatting fixes. If this was a standalone python script I'd be done

--- getcomments.py
+++ getcomments.py
@@ -1,22 +1,19 @@
 import praw
-
-Player_Okafor = ('okafor','okafor')
-Player_Towns = ('towns','towns')
-Player_Winslow = ('winslow','winslow')
-Player_Mudiay = ('Mudiay', 'mudiay')
-Player_Russell = ("D'Angelo",'russell')
-Player_Porzingis = ('Porzingis','porzingis')
-Player_WCS = ('Willie','wcs')
-Player_Hezonja = ('Hezonja','hezonja')
-Player_Stanley = ('Stanley Johnson','johnson')
+import requests
+import json
+import datetime
+import time
+from vardata import *
 
 
-player = Player_Mudiay
+player = Player_Russell
 #What player are we searching for?
+timeFrame = month
+#How far back are we looking?
 
 r = praw.Reddit('Comment Scraper for DraftStocked')
-subreddit = r.get_subreddit('nba')
-submissions = subreddit.search(player[0], sort='relevance',period='month')
+multi_reddits = r.get_subreddit('nba+NBA_Draft')
+submissions = multi_reddits.search(player[0], sort='relevance',period=timeFrame)
 
 comment_list = []
 for x in submissions:
@@ -30,15 +27,50 @@ for x in submissions:
 			#print(comment.body)
 			#print('\n')
 			#Debug stuff
-			comment_list.append((comment.score,comment.body,comment.permalink))
+			comment_list.append((comment.score,comment.body,comment.permalink,comment.created_utc))
 
 
-print("AGGREGATED COMMENTS: ")
+print("\nAGGREGATED COMMENTS:\n")
 
 comment_list.sort(key=lambda x: x[0], reverse=True)
 
-for sc,bd,pm in comment_list:
-	print(pm)
-	print(bd+'\n')
+playerStock = 0
+#Initialize player stock
+
+displayAmount = 25
+#How many comments to display?
+timeFilter = secondsMonth
+#View comments from the past ____ 
+
+currentTime = int(time.time())
+#tuple is of comment score, comment body, comment permalink, comment date posted
+for sc,bd,pm,dt in comment_list:
+	#Post request to semantic analysis API. This is how we're determining whether a 
+	#comment is helping or detrimental to the players stock.
+	response = requests.post("https://twinword-sentiment-analysis.p.mashape.com/analyze/",
+  	headers={
+    "X-Mashape-Key": "V1YkqJPoHwmshbjpJICAEF9E2ublp1W2P3qjsnfbuwdxTHdUTj",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "application/json"
+  	},
+  	params={
+    "text": bd
+  	}
+	)
+	playerStock+=response.json()["score"]
+	#print(response.json())
+
+	if(displayAmount>=0):
+		if(currentTime-dt < timeFilter and sc > 0):
+			print("COMMENT #"+str(25-displayAmount))
+			print('Context: '+pm)
+			print('Comment Score: '+str(sc))
+			print("Date Posted:"+datetime.datetime.fromtimestamp(int(dt)).strftime('%Y-%m-%d %H:%M:%S'))
+			print(bd+'\n')
+			displayAmount+= -1
+	else:
+		print('.', end="", flush=True)
+
+print("\nPlayer stock of "+player[2]+" is %.2f" % round(playerStock,2))
 
 
---
+++ vardata.py
@@ -0,0 +1,27 @@
+#Player Database
+Player_Okafor = ('okafor','okafor','Jahlil Okafor')
+Player_Towns = ('towns','towns','Karl Anthony-Towns')
+Player_Winslow = ('winslow','winslow','Justice Winslow')
+Player_Mudiay = ('Mudiay', 'mudiay', 'Emmanuel Mudiay')
+Player_Russell = ("D'Angelo",'russell',"D'Angelo Russell")
+Player_Porzingis = ('Porzingis','porzingis','Kristaps Porzingis')
+Player_WCS = ('Willie','wcs','Willie Cauley-Stein')
+Player_Hezonja = ('Hezonja','hezonja','Mario Hezonja')
+Player_Stanley = ('Stanley Johnson','johnson','Stanley Johnson')
+#Variables are tuples of 3: (A,B,C) where:
+#A = reddit search term. something distinguishable
+#B = nickname or shorthand, what he is usually called
+#C = Full name 
+
+
+#Time parameters for API
+year = 'year'
+month = 'month'
+week = 'week'
+day = 'day'
+
+#for filtering comments (UNIX timestamp subtraction)
+secondsYear = 31536000
+secondsMonth = 2678400 
+secondsWeek = 604800
+secondsDay = 86400
