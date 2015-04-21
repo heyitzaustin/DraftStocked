961a7a64802504f424584995c36a7e99fab5bc9f
Austin Li <austinli@Austins-MacBook-Pro.local>
2015-04-20 17:37:04 -0700

    improved search parameters. have cases for 10 players(taken from mock draft)

--- getcomments.py
+++ getcomments.py
@@ -1,9 +1,22 @@
 import praw
 
+Player_Okafor = ('okafor','okafor')
+Player_Towns = ('towns','towns')
+Player_Winslow = ('winslow','winslow')
+Player_Mudiay = ('Mudiay', 'mudiay')
+Player_Russell = ("D'Angelo",'russell')
+Player_Porzingis = ('Porzingis','porzingis')
+Player_WCS = ('Willie','wcs')
+Player_Hezonja = ('Hezonja','hezonja')
+Player_Stanley = ('Stanley Johnson','johnson')
+
+
+player = Player_Mudiay
+#What player are we searching for?
+
 r = praw.Reddit('Comment Scraper for DraftStocked')
-#r.login('Draftstocked','shonen')
 subreddit = r.get_subreddit('nba')
-submissions = subreddit.search('okafor', sort='top',period='month')
+submissions = subreddit.search(player[0], sort='relevance',period='month')
 
 comment_list = []
 for x in submissions:
@@ -13,18 +26,19 @@ for x in submissions:
 	submission.replace_more_comments(limit=16, threshold=10)
 	flat_comments = praw.helpers.flatten_tree(submission.comments)
 	for comment in flat_comments:
-		if " he " in comment.body and comment.id not in comment_list:
-			print('COMMENT:')
-			print(str(comment))
-			comment_list.append((comment.score,comment.body))
+		if player[1] in (comment.body).lower() and comment.id not in comment_list:
+			#print(comment.body)
+			#print('\n')
+			#Debug stuff
+			comment_list.append((comment.score,comment.body,comment.permalink))
 
 
 print("AGGREGATED COMMENTS: ")
 
 comment_list.sort(key=lambda x: x[0], reverse=True)
 
-for sc,bd in comment_list:
-	print(sc)
-	print(bd)
+for sc,bd,pm in comment_list:
+	print(pm)
+	print(bd+'\n')
 
 
