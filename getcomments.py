8cf30a3023767bf80fbda4bb1e8dd5a4fb1a63f2
heyitzaustin <heyitzaustin@gmail.com>
2015-04-24 02:17:06 -0700

    added comments for clarity

--- getcomments.py
+++ getcomments.py
@@ -15,41 +15,41 @@ def ParseComments(player):
 	timeFrame = month
 	#How far back are we looking?
 
-	r = praw.Reddit('Comment Scraper for DraftStocked')
+	r = praw.Reddit('Comment Scraper for DraftStocked by u/heyitzaustin')
 	multi_reddits = r.get_subreddit('nba+NBA_Draft')
-
+	#looking at these two subreddits for now so we're sure to get relevant content
 	submissions = multi_reddits.search(player.search, sort='relevance',period=timeFrame)
-
+	#getting the submissions!
+	
 	print("\nDraft Stock report on "+player.fullname+"\n")
 	comment_list = []
+	
 	for x in submissions:
 		print(str(x))
 		print(x.id)
+		#Debug stuff
 		submission = r.get_submission(submission_id=x.id)
 		submission.replace_more_comments(limit=0, threshold=0)
-		# ^ This is our bottleneck. See if we can make it faster?
+		# ^ we don't really care about lower level/reply comments. Set limit to 0 so fewer API calls
 		flat_comments = praw.helpers.flatten_tree(submission.comments)
 		for comment in flat_comments:
 			if player.nickname in (comment.body).lower() and comment.id not in comment_list:
-				#print(comment.body)
-				#print('\n')
-				#Debug stuff
 				comment_list.append(Comment(comment.score,comment.body,comment.permalink,comment.created_utc))
+				#if the player was mentioned in the comment, create a comment object and add to our list
 		
 
 
+	#We got the comments! Now let's sort them
 	print("\nAGGREGATED COMMENTS:\n")
 
 	comment_list.sort(key=lambda x: x.score, reverse=True)
-
+	#sorting my score/Karma
 	playerStock = 0
 	#Initialize player stock
-
 	displayAmount = 25
 	#How many comments to display?
 	timeFilter = secondsMonth
 	#View comments from the past ____ 
-
 	currentTime = int(time.time())
 	#Current time for comparison
 
@@ -68,8 +68,9 @@ def ParseComments(player):
   		}
 		)
 		playerStock+=response.json()["score"]
-		#print(response.json())
-
+		#increment the player's stock by the semantic analysis score
+		
+		#Just debug stuff. For terminal use so it doesn't get overcrowded
 		if(displayAmount>=0):
 			if(currentTime-comment.date_posted < timeFilter and comment.score > 0):
 				print("COMMENT #"+str(25-displayAmount))
@@ -80,15 +81,19 @@ def ParseComments(player):
 				displayAmount+= -1
 		else:
 			print('.', end="", flush=True)
+			#Let's us know the script is still running
 
 		with con:
 			params = (player.fullname,comment.score, comment.body, comment.permalink, comment.date_posted)
 			cur = con.cursor()
 			cur.execute("INSERT INTO Comments VALUES(?,?,?,?,?)",params)
+			#Add to our local SQL database!
 
 	with con:
 		cur = con.cursor()
 		cur.execute("UPDATE Players SET Stock=? WHERE Fullname=?", (round(playerStock,2), player.fullname)) 
+		#update our player info to have new stock
+		
 	print("\nPlayer stock of "+player.fullname+" is %.2f" % round(playerStock,2))
 
 def getComments(player):
